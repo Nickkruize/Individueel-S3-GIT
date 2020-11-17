a6a68790f1e2840babab6b98112eb810af7598b5
Nick Kruize <n.kruize@gmail.com>
2020-11-17 01:03:01 +0100

    Generalized test context

--- S3 IGDB/InternetGameDatabase/IGDB Tests/TestContext.cs
+++ S3 IGDB/InternetGameDatabase/IGDB Tests/TestContext.cs
@@ -1,52 +1,32 @@
-﻿//using DAL;
-//using DAL.ContextModel;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System;
-//using System.Collections.Generic;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using DAL;
+using DAL.ContextModel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace IGDB_Tests
-//{
-//    [TestClass]
-//    public class TestContext
-//    {
-//        public async Task<IGDBContext> GetDatabaseContext()
-//        {
-//            var options = new DbContextOptionsBuilder<IGDBContext>().UseInMemoryDatabase(databaseName: "testdb").Options;
-//            var databaseContext = new IGDBContext(options);
-//            databaseContext.Database.EnsureCreated();
+namespace IGDB_Tests
+{
+    public class TestContext
+    {
+        public async Task<IGDBContext> GetDatabaseContext<T>(List<T> data) where T : class
+        {
+            var options = new DbContextOptionsBuilder<IGDBContext>().UseInMemoryDatabase(databaseName: "testdb").EnableSensitiveDataLogging().Options;
+            var databaseContext = new IGDBContext(options);
+            databaseContext.Database.EnsureCreated();
 
-//            if (await databaseContext.Users.CountAsync() <= 0)
-//            {
-//                foreach (User user in Userdata())
-//                {
-//                    databaseContext.Users.Add(user);
-//                }
-//                await databaseContext.SaveChangesAsync();
-//            }
+            if (await databaseContext.Set<T>().CountAsync() <= 0)
+            {
+                for (int i = 0; i < data.Count; i++)
+                {
+                    databaseContext.Set<T>().Add(data[i]);
+                }
+                await databaseContext.SaveChangesAsync();
+            }
 
-//            return databaseContext;
-//        }
-
-//        public IEnumerable<User> Userdata()
-//        {
-//            List<User> users = new List<User>();
-
-//            for (int i = 0; i < 10; i++)
-//            {
-//                User user = new User()
-//                {
-//                    Id = i,
-//                    Email = $"testuser{i}@tester.com",
-//                    Password = $"test{i}",
-//                    Role = Roles.User
-//                };
-//                users.Add(user);
-//            }
-
-//            return users;
-//        }
-//    }
-//}
+            return databaseContext;
+        }
+    }
+}
--- S3 IGDB/InternetGameDatabase/IGDB Tests/Usertests.cs
+++ S3 IGDB/InternetGameDatabase/IGDB Tests/Usertests.cs
@@ -1,8 +1,6 @@
 ﻿using DAL;
 using DAL.ContextModel;
 using IGDB_Users.Controllers;
-using IGDB_Users.ModelConverter;
-using IGDB_Users.Models;
 using IGDB_Users.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,31 +8,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using DeepEqual.Syntax;
 using Microsoft.AspNetCore.Mvc;
-using System.Net;
 
 namespace IGDB_Tests
 {
     [TestClass]
-    public class Usertests
+    public class Usertests : TestContext
     {
-        public async Task<IGDBContext> GetDatabaseContext()
-        {
-            var options = new DbContextOptionsBuilder<IGDBContext>().UseInMemoryDatabase(databaseName: "testdb").EnableSensitiveDataLogging().Options;
-            var databaseContext = new IGDBContext(options);
-            databaseContext.Database.EnsureCreated();
-
-            if (await databaseContext.Users.CountAsync() <= 0)
-            {
-                for (int i = 0; i < Userdata().Count; i++)
-                {
-                    databaseContext.Users.Add(Userdata()[i]);
-                }
-                await databaseContext.SaveChangesAsync();
-            }
-
-            return databaseContext;
-        }
-
         public List<User> Userdata()
         {
             List<User> users = new List<User>();
@@ -58,20 +37,18 @@ namespace IGDB_Tests
         [TestMethod]
         public void GetAllUser()
         {
-            var dbContext = GetDatabaseContext().Result;
+            var dbContext = GetDatabaseContext<User>(Userdata()).Result;
             UsersController controller = new UsersController(new UserRepository(dbContext));
-            var expected = Userdata();
-            var result = dbContext.Users.ToListAsync();
+            List<User> result = dbContext.Users.ToListAsync().Result;
 
             Assert.IsNotNull(controller.Get());
-            Assert.IsTrue(Userdata().IsDeepEqual(dbContext.Users.ToListAsync().Result));
+            Assert.IsTrue(Userdata().IsDeepEqual(result));
         }
 
         [TestMethod]
         public void GetUserByIdUserExistsLowerBoundary()
         {
-            var dbContext = GetDatabaseContext().Result;
-            UsersController controller = new UsersController(new UserRepository(dbContext));
+            var dbContext = GetDatabaseContext<User>(Userdata()).Result;
             User expected = Userdata()[0];
             User result = dbContext.Users.Find(1);
 
@@ -82,8 +59,7 @@ namespace IGDB_Tests
         [TestMethod]
         public void GetUserByIdUserExistsUpperBoundary()
         {
-            var dbContext = GetDatabaseContext().Result;
-            UsersController controller = new UsersController(new UserRepository(dbContext));
+            var dbContext = GetDatabaseContext<User>(Userdata()).Result;
             User expected = Userdata()[9];
             User result = dbContext.Users.Find(10);
 
@@ -94,18 +70,18 @@ namespace IGDB_Tests
         [TestMethod]
         public void GetUserByIdUserDoesNotExistLower()
         {
-            var dbContext = GetDatabaseContext().Result;
+            var dbContext = GetDatabaseContext<User>(Userdata()).Result;
             UsersController controller = new UsersController(new UserRepository(dbContext));
-            var reuslt = controller.Get(0).Result;
+            var result = controller.Get(0).Result;
 
             Assert.IsNull(dbContext.Users.Find(0));
-            Assert.IsInstanceOfType(reuslt, typeof(NotFoundResult));
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
 
         [TestMethod]
         public void GetUserByIdUserDoesNotExistUpper()
         {
-            var dbContext = GetDatabaseContext().Result;
+            var dbContext = GetDatabaseContext<User>(Userdata()).Result;
             UsersController controller = new UsersController(new UserRepository(dbContext));
             var reuslt = controller.Get(11).Result;
 
